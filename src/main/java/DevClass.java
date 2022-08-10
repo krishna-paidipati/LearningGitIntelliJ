9934c066db3ad64673c0eb075edc4bbee684af1c
VijKrish <52032539+krishna-paidipati@users.noreply.github.com>
2022-08-10 12:42:54 +0100

    Commit3 (#3)
    Adding stash branch changes from vijay

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,12 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
+        System.out.println("DevClass VijayGit 123");
         //checkin from vijay Git
         // added 123
     }
