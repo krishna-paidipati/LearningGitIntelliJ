34777b4ce5550f513d5e2b736c8511d6937c3e60
vijkrishp <110944632+vijkrishp@users.noreply.github.com>
2022-08-10 01:16:24 +0100

    added 123
    added 123

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,7 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass VijayGit");
+        System.out.println("DevClass VijayGit 123");
         //checkin from vijay Git
+        // added 123
     }
 }
